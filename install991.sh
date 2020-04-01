386fe104c113a3b0c5114f29f70fee9c95babb98
Ranjan <58366704+rjn991@users.noreply.github.com>
2020-04-01 18:28:10 +0530

    Create install991.sh

---
+++ install991.sh
@@ -0,0 +1,36 @@
+#!/data/data/com.termux/files/usr/bin/bash
+# rjn991
+ 
+echo -e "Updating default packages\n"
+apt update && apt -y upgrade
+ 
+echo -e "Requesting acces to storage\n"
+termux-setup-storage
+sleep 5
+ 
+echo -e "Installing python\n"
+apt install python
+ 
+echo -e "Installing youtube-dl\n"
+pip install youtube-dl
+ 
+echo -e "Creating the Youtube folder to download the files\n"
+mkdir ~/storage/shared/Youtube
+ 
+echo -e "Creating youtube-dl folder for config\n"
+mkdir -p ~/.config/youtube-dl
+
+echo -e "Moving config file"
+dos2unix /data/data/com.termux/files/home/config 
+mv -f /data/data/com.termux/files/home/config /data/data/com.termux/files/home/.config/youtube-dl
+ 
+echo -e "Creating bin folder\n"
+mkdir ~/bin
+ 
+echo -e "Downloading and installing termux-url-opener\n"
+mv -f /data/data/com.termux/files/home/termux-url-opener /data/data/com.termux/files/home/bin 
+dos2unix ~/bin/termux-url-opener
+echo -e "\n"
+echo -e "\n"
+echo -e "Downloaded Files will be found in Youtube Folder in internal storage \n"
+echo "rjn991"
